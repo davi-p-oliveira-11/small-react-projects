b60187da35d0d6885f09e5bbf5f4c7ccb2d5f40f
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-12 15:39:41 -0300

    chore(deps): add axios for HTTP requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // import Counter from "./components/Counter/Counter";
 // import TodoList from "./components/TodoList/TodoList";
 
+import Meals from "./components/Meals/Meals";
+
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <TodoList /> */}
+      <Meals />
     </div>
   );
 };
