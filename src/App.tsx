a3401a22498aa038a635834bd588f9eb452ef223
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-12 18:58:30 -0300

    feat(search-bar): implement complete SearchBar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 // import TodoList from "./components/TodoList/TodoList";
 // import Meals from "./components/Meals/Meals";
 // import Calculator from "./components/Calculator/Calculator";
-
-import ToggleComponent from "./components/ToggleBackgroundColor/ToggleComponent";
+// import ToggleComponent from "./components/ToggleBackgroundColor/ToggleComponent";
+import HiddenSearch from "./components/HIddenSearch/HiddenSearch";
 
 const App = () => {
   return (
@@ -12,7 +12,8 @@ const App = () => {
       {/* <TodoList /> */}
       {/* <Meals /> */}
       {/* <Calculator /> */}
-      <ToggleComponent />
+      {/* <ToggleComponent /> */}
+      <HiddenSearch />
     </div>
   );
 };
