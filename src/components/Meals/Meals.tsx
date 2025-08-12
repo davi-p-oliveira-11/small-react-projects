2c37981fb6982db9b05a52a9aa75df3f66ea4417
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-12 15:58:06 -0300

    feat(meal): complete Meal component implementation

--- src/components/Meals/Meals.tsx
+++ src/components/Meals/Meals.tsx
@@ -3,9 +3,33 @@ import { useState, useEffect } from "react";
 import "./meals.css";
 
 const Meals: React.FC = () => {
-  return (
-    <div>Meals</div>
-  )
-}
+  const [items, setItems] = useState([]);
 
-export default Meals
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        //   console.log(res.data.meals);
+        setItems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemsList = items.map(({strMeal, strMealThumb, idMeal}) => {
+     return (
+       <section className="card">
+         <img src={strMealThumb} alt={strMeal} /> 
+         <section className="content">
+           <p>{strMeal}</p>
+           <p>#{idMeal}</p>
+         </section>
+       </section>
+     )
+  })
+
+  return <div className="items-container">{itemsList}</div>;
+};
+
+export default Meals;
