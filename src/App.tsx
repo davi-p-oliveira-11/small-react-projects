2f8df32564f9113c19ee50acfa7700544cab482f
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-12 16:06:13 -0300

    feat(calculator): add initial Calculator component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 // import Counter from "./components/Counter/Counter";
 // import TodoList from "./components/TodoList/TodoList";
+// import Meals from "./components/Meals/Meals";
 
-import Meals from "./components/Meals/Meals";
+import Calculator from "./components/Calculator/Calculator";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <TodoList /> */}
-      <Meals />
+      {/* <Meals /> */}
+      <Calculator />
     </div>
   );
 };
